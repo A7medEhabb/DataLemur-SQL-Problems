0d6259ea63bebc5f3a82dd10b4ff2ca5ed130532
Ahmed Ehab <144546364+A7medEhabb@users.noreply.github.com>
2026-03-09 19:52:42 +0200

    Create Spotify Streaming History.sql

---
+++ Problems/Spotify Streaming History.sql
@@ -0,0 +1,24 @@
+WITH CTE AS (
+  SELECT
+    user_id,
+    song_id,
+    song_plays
+  FROM songs_history
+  UNION ALL
+  SELECT
+    user_id,
+    song_id,
+    COUNT(song_id) as song_plays
+  FROM
+    songs_weekly w
+  WHERE
+    listen_time <= '08/04/2022 23:59:59'
+  GROUP BY user_id, song_id
+)
+
+SELECT 
+  user_id, song_id, 
+  SUM(song_plays) AS song_count
+FROM cte 
+GROUP BY user_id, song_id 
+ORDER BY song_count DESC;
